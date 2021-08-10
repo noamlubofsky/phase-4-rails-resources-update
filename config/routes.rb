fd5e00617be623bc5afbb8a1702c0b6894f1d80e
Noam Lubofsky <noamlubofsky@gmail.com>
2021-08-10 10:18:50 -0400

    commit tree 7b3a21b3089defb41187113dd6a1409147273f0b

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create]
+  resources :birds, only: [:index, :show, :create, :update]
+  patch "/birds/:id/like", to: "birds#increment_likes"
+
+  def increment_likes
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+  
 end
